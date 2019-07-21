e104d8a61030835080537e6200d35d40fce8d52c
Tom Bawel <tbawel@gmail.com>
2019-07-21 17:46:01 -0600

    minor auth changes

--- app/Middleware/BingeAuth.js
+++ app/Middleware/BingeAuth.js
@@ -9,13 +9,9 @@ class BingeAuth {
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ request }, next) {
+  async handle({ request, auth }, next) {
     // call next to advance the request
-    try {
-      return await auth.getUser()
-    } catch (error) {
-      response.send('Missing or invalid jwt token')
-    }
+    await auth.getUser()
     await next()
   }
 }
